f23d576136b74f0e44370c09c7f37d4d214a3905
RomanNitefor <nitefor.roma@gmail.com>
2024-04-13 10:19:16 +0300

    complete-task-2

--- js/task-2.js
+++ js/task-2.js
@@ -10,5 +10,19 @@ const images = [
   {
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
-  }
+  },
 ];
+const listImages = document.querySelector('.gallery');
+
+images.forEach(image => {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.src = image.url;
+  img.alt = image.alt;
+  img.style.width = '360px';
+  img.style.height = '300px';
+  li.append(img);
+  listImages.append(li);
+});
+
+console.log('listImages:', listImages);
