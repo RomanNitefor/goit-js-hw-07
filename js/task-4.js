bf475d50f1bea00069afd14a739443d03b01738a
RomanNitefor <nitefor.roma@gmail.com>
2024-04-13 10:56:29 +0300

    complete-task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
+const onFormSubmit = event => {
+  event.preventDefault();
+  const formEl = event.currentTarget.elements;
+  const info = {
+    email: formEl.email.value,
+    pass: formEl.password.value,
+  };
+  if (info.email === '' || info.pass === '') {
+    return alert('All form fields must be filled in');
+  }
+  console.log(info);
+  event.currentTarget.reset();
+};
 
+form.addEventListener('submit', onFormSubmit);
