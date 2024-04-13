be229fb870bab3b16dbb7bca9f983011aec2c13b
RomanNitefor <nitefor.roma@gmail.com>
2024-04-13 15:40:06 +0300

    final-commit

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const controlsDiv = document.getElementById('controls');
+const createBtn = controlsDiv.querySelector('[data-create]');
+const destroyBtn = controlsDiv.querySelector('[data-destroy]');
+const boxesDiv = document.getElementById('boxes');
+const input = controlsDiv.querySelector('input');
+
+function createBoxes(amount) {
+  let size = 30;
+  boxesDiv.innerHTML = '';
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesDiv.appendChild(box);
+    size += 10;
+    box.classList.add('generate-new');
+  }
+  input.value = '';
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const inputValue = Number(input.value);
+  createBoxes(inputValue);
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
